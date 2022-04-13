b227404461cfd57a46ccd72fffb7408584b01eea
Yopa <yopa.cullun@gmail.com>
2022-04-14 00:40:33 +0700

    Initial Commit

---
+++ Kelassik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kelassik
+//
+//  Created by yopa bagus on 14/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
